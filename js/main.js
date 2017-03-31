bf2128670fe902c8d1fd0e9c67232bb2cab60593
wimjjj <wim.jongeneel1@gmail.com>
2017-03-31 21:09:23 +0200

    added timet

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ var foundItems = [];
 /**
  * add the onClick listerners to the items
  */
-$( ".item" ).click(function(e) {
-    if(foundItems.indexOf(e.target.id) !== -1) return;
+$(".item").click(function (e) {
+    if (foundItems.indexOf(e.target.id) !== -1) return;
 
     e.target.classList.add("greyscale", "pop");
 
@@ -23,21 +23,31 @@ $( ".item" ).click(function(e) {
 });
 
 /**
+ * updates the timer every 1s with 1
+ */
+setInterval(function () {
+    var elem = $("#time");
+    var count = Number(elem.html());
+    count++;
+    elem.html(count);
+}, 1000);
+
+/**
  * shows the big messageBox.
  * will remove all other messageBoxes show
  * messageBox wil be removed after 2.5s
  * @param msg
  */
-function showMessageBox(msg){
+function showMessageBox(msg) {
     var messageBoxes = $(".messageBox");
-    if(messageBoxes.length) messageBoxes.remove();
+    if (messageBoxes.length) messageBoxes.remove();
 
     var elem = document.createElement('p');
     elem.classList.add("messageBox");
     elem.innerHTML = msg;
     document.body.append(elem);
 
-    setTimeout(function(){
+    setTimeout(function () {
         this.elem.remove();
     }.bind({elem}), 2500);
 }
@@ -45,10 +55,10 @@ function showMessageBox(msg){
 /**
  * updates the counter by 1
  */
-function updateCounter(){
- var elem = $("#counter");
- var count = elem.html().substr(0, elem.html().indexOf("/"));
- count++;
+function updateCounter() {
+    var elem = $("#counter");
+    var count = elem.html().substr(0, elem.html().indexOf("/"));
+    count++;
 
- elem.html(count + elem.html().substr(elem.html().indexOf("/")));
+    elem.html(count + elem.html().substr(elem.html().indexOf("/")));
 }

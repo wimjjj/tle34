b327885869828acd8510a6782ead4b3376fcf894
wimjjj <wim.jongeneel1@gmail.com>
2017-05-30 12:21:11 +0200

    added scorespage

--- js/scores.js
+++ js/scores.js
@@ -1,6 +1,6 @@
 var elem = document.getElementById('scores');
 
-$.getJson('/api/scores.php').then((result) => {
+$.getJSON('/api/scores.php').then((result) => {
     var scores = result.data;
 
     console.log(scores);

236da22ab4a8e918b560ec8764c6c8e63121f0cf
wimjjj <wim.jongeneel1@gmail.com>
2017-05-30 11:01:36 +0200

    php

--- api/score.php
+++ api/score.php
@@ -6,45 +6,41 @@
      * Time: 15:24
      */
      
-     try {
-        require('dbconnect.php');
+    require('dbconnect.php');
          
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $db);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    function add($conn, $name, $score){
+        $name = mysqli_real_escape_string($conn, $name);
+        $score = mysqli_real_escape_string($conn, $score);
+        $date = date("d-m-Y");
+        $query = "INSERT INTO scores (name, score, date) VALUES (". $name .  ", ". $score . ", ". $date .")";
+
+        $result = $conn->query($query);
+
+    }
+
+/**
+ * @param $conn
+ * @return string
+ */
+    function view($conn) {
+        $array = [];
+
+        $query = "SELECT name, score, date FROM 'scores' ORDER BY score LIMIT 25";
+        $result = $conn->query($query);
+
+        while ($row = $result->fetch_assoc())
+        {
+            $array[] = $row;
         }
-        echo "Connected successfully";
-     } catch (Exception $e) {
-         echo $e->getMessage();
-     }
 
-//     function add($conn, $name, $score){
-//         $name = mysqli_real_escape_string($conn, $name);
-//         $score = mysqli_real_escape_string($conn, $score);
-//         $date = date("d-m-Y");
-//         $query = "INSERT INTO scores (name, score, date) VALUES (". $name .  ", ". $score . ", ". $date .")";
-
-//         $result = $conn->query($query);
-
-//     }
-
-// /**
-//  * @param $conn
-//  * @return string
-//  */
-//     function view($conn) {
-//         $array = [];
-
-//         $query = "SELECT name, score, date FROM 'scores' ORDER BY score LIMIT 25";
-//         $result = $conn->query($query);
-
-//         while ($row = $result->fetch_assoc())
-//         {
-//             $array[] = $row;
-//         }
-
-//         return json_encode($array);
-//     }
+        return json_encode($array);
+    }
